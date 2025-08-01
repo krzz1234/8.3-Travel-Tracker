40439ddb247f332d598777fe69b7f2fa22cb3c6e
krzz1234 <89624081+krzz1234@users.noreply.github.com>
2025-08-01 18:08:16 +1000

    new commit

--- index.js
+++ index.js
@@ -25,7 +25,24 @@ app.get("/", async (req, res) => {
   });
   console.log(countries);
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  let input = req.body.country;
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
